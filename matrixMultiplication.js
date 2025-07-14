6c68e24e354b97f101273e9d0c6dab44be66c104
dannyboym <danboy94583@gmail.com>
2025-07-14 15:08:08 +0200

    Mulplication of Arrays

---
+++ matrixMultiplication.js
@@ -0,0 +1,15 @@
+function matrixMultiplication(a, b) {
+    const result = [];
+  for(let i=0;i<a.length; i++){
+    result[i] = [];
+    for(let j=0;j<b[0].length;j++){
+      let sum = 0;
+      for(let k=0;k<a[0].length;k++){
+        sum += a[i][k] * b[k][j];
+      }
+      result[i][j] = sum;
+    }
+  }
+  return result;
+}
+console.log(matrixMultiplication([[1, 2], [3, 2]], [[3, 2], [1, 1]]));
